9ffa90dac05b9f9507b7f3e27cba11f5b10a1649
Giome <guillaume.paulet@etu.univ-nantes.fr>
2016-04-25 00:15:57 +0200

    [UT] ResourceManager import sprites from JSON file well.

--- src/ResourceManager/mainres.cpp
+++ src/ResourceManager/mainres.cpp
@@ -10,11 +10,13 @@ int main()
 
 	rm.loadAll();
 
+	auto sp1 = rm.get("zombie_1");
+	std::cout << sp1->name() << std::endl;
 
-	auto sp = rm.get("zombie");
+	auto sp2 = rm.get("zombie_2");
+	std::cout << sp2->name() << std::endl;
 
-
-
-	std::cout << sp->name() << std::endl;
+	auto sp3 = rm.get("zombie_3");
+	std::cout << sp3->name() << std::endl;
 }
 
